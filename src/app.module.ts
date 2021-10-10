07cd03f896f36b4e03713d2a18bf6b92428d2391
fvzzini <b.knvpik@gmail.com>
2021-10-10 23:31:20 +0200

    Integration with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,21 @@ import { UsersModule } from './users/users.module';
 import { ItemsModule } from './items/items.module';
 import { OffersModule } from './offers/offers.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection, getConnectionOptions } from 'typeorm';
+
+TypeOrmModule.forRootAsync({
+  useFactory: async () =>
+    Object.assign(await getConnectionOptions(), {
+      autoLoadEntities: true,
+    }),
+});
 
 @Module({
-  imports: [UsersModule, ItemsModule, OffersModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(), UsersModule, ItemsModule, OffersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
